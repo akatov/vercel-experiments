3cfb7f3830ee5e75833ed5e8df137d07900772f3
Dmitri Akatov <dmitri@akatov.com>
2023-08-17 00:08:30 +0100

    some additional logging

--- src/main/api.cljs
+++ src/main/api.cljs
@@ -2,6 +2,12 @@
   (:require ["cowsay" :refer (say)]))
 
 (defn ^:export main [^js/Request req ^js/Response res]
+  (js/console.debug "API Endpoint Hit (debug)")
+  (js/console.log "API Endpoint Hit (log)")
+  (js/console.info "API Endpoint Hit (info)")
+  (js/console.warn "API Endpoint Hit (warn)")
+  (js/console.error "API Endpoint Hit (error)")
+  (println "request:" req)
   (let [name    (or (.. req -query -name) "friend")
         version js/process.version
         url     (.-url req)
@@ -11,6 +17,7 @@
                      "Request URL: " url "\n"
                      "Server time: " time)
         body    (say #js {:text text})]
+    (println "about to return body:\n" body)
     (doto res
       (.setHeader "Content-Type" "text/plain")
       (.end body)))
--- src/main/app.cljs
+++ src/main/app.cljs
@@ -1,6 +1,7 @@
 (ns app)
 
 (defn ^:export ^:dev/after-load init! []
+  (js/console.log "Hello World!")
   (println "Initialized" ::module))
 
 (defn ^:export ^:dev/before-load halt! []
