bc6e974ecd37f3eea5b9db07ff20824efd3ecd8a
Dmitri Akatov <dmitri@akatov.com>
2023-08-17 15:31:22 +0100

    use macchiato as http handler

--- src/main/api.cljs
+++ src/main/api.cljs
@@ -1,16 +1,20 @@
 (ns api
-  (:require ["cowsay" :refer (say)]))
+  (:require
+   [macchiato.http :as http]
+   [macchiato.middleware.defaults :refer [site-defaults
+                                          wrap-defaults]]
+   [macchiato.util.response :as r]
+   ["cowsay" :refer (say)]))
 
-(defn ^:export main [^js/Request req ^js/Response res]
+(defn handler [req res raise]
   (js/console.debug "API Endpoint Hit (debug)")
   (js/console.log "API Endpoint Hit (log)")
   (js/console.info "API Endpoint Hit (info)")
   (js/console.warn "API Endpoint Hit (warn)")
   (js/console.error "API Endpoint Hit (error)")
-  (println "request:" req)
-  (let [name    (or (.. req -query -name) "friend")
+  (let [name    (get-in req [:query-params "name"] "friend")
         version js/process.version
-        url     (.-url req)
+        url     (get req :url)
         time    (.toISOString (js/Date.))
         text    (str "Howdy, " name ", from Vercel!\n"
                      "Node.js: " version "\n"
@@ -18,7 +22,11 @@
                      "Server time: " time)
         body    (say #js {:text text})]
     (println "about to return body:\n" body)
-    (doto res
-      (.setHeader "Content-Type" "text/plain")
-      (.end body)))
-  js/undefined)
+    (-> (r/ok body)
+        (r/content-type "text/plain")
+        res)))
+
+(def ^:export main
+  (-> handler
+      (wrap-defaults site-defaults)
+      (http/handler {})))
